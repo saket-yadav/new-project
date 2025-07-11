4c11a9fa6cab5a7b05ff959b21c21308e7f0b41c
saket-yadav <saketyadav741@gmail.com>
2025-07-11 07:25:10 +0545

    initial commit

---
+++ quick.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class quick {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100) + 1; // random number between 1 and 100
+        System.out.println("Random number: " + randomNum);
+    }
+}
